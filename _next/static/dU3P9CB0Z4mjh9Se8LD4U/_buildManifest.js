a22d19834d2e8e60f40586df7db3c005aec74383
mitrank <mitrank@users.noreply.github.com>
2025-11-03 21:01:12 +0000

    deploy: fe4c93e62643ca68ea4630e280d314137d31d953

--- _next/static/dU3P9CB0Z4mjh9Se8LD4U/_buildManifest.js
+++ _next/static/dU3P9CB0Z4mjh9Se8LD4U/_buildManifest.js
@@ -1,15 +1,10 @@
 self.__BUILD_MANIFEST = {
   "/_error": [
-    "./static/chunks/46e8002f8dc5b6cb.js"
+    "./static/chunks/325b9aaef4d40521.js"
   ],
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/portfolio//_next/:path+",
-        "destination": "/portfolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
