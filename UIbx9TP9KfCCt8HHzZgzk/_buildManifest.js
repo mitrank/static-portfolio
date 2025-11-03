1c6c991c49a419138edd5cdf634ffd02c71e113c
mitrank <mitrankshah@yahoo.com>
2025-11-04 02:11:48 +0530

    Updates

---
+++ UIbx9TP9KfCCt8HHzZgzk/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/0cd6ef8b858554d1.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio//_next/:path+",
+        "destination": "/portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
