44b059886182bc26b32f0c3349be9104a219ebcc
mitrank <mitrank.shah@think360.ai>
2025-11-04 14:41:05 +0530

    Updates

--- _next/static/chunks/679bc0ba96381e09.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/ef1c32ad4cdb6f78.js",
-  "static/chunks/turbopack-fa4e1f8d329c0029.js"
-])
---
+++ _next/static/chunks/a8548f6d714b005b.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/282a645eb73ce027.js",
+  "static/chunks/turbopack-2e2950c805cf244f.js"
+])
